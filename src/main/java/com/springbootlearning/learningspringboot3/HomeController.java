c6845193b5c28e88e9c0f75f14b39597fdd6b35b
Kelly Netterville <36lima@gmail.com>
2023-12-29 15:32:00 -0600

    Added Model to index() method and dynamic language to iterate through list of videos.

--- src/main/java/com/springbootlearning/learningspringboot3/HomeController.java
+++ src/main/java/com/springbootlearning/learningspringboot3/HomeController.java
@@ -1,14 +1,23 @@
 package com.springbootlearning.learningspringboot3;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 @Controller
 public class HomeController {
+    record Video(String name) {}
+    List<Video> videos = List.of(
+            new Video("Need HELP with your Spring Boot 3 App?"),
+            new Video("Don't do THIS to your own CODE!"),
+            new Video("SECRETS to fix BROKEN CODE!"));
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("videos", videos);
         return "index";
-    }  
+   }
 }
